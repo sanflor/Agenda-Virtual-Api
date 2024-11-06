49a6ce4871d68083361f8d7d91f7220a7ceda495
sanflor <902olbalarconsantiago@gmail.com>
2024-11-06 18:34:52 -0500

    creacion de test del controlador

--- src/test/java/com/todotic/contactlistapi/controller/ContactControllerTest.java
+++ src/test/java/com/todotic/contactlistapi/controller/ContactControllerTest.java
@@ -1,108 +1,222 @@
 package com.todotic.contactlistapi.controller;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.todotic.contactlistapi.dto.ContactDTO;
 import com.todotic.contactlistapi.entity.Contact;
 import com.todotic.contactlistapi.service.ContactService;
-import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.aot.DisabledInAotMode;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@ExtendWith(MockitoExtension.class)
-@WebMvcTest(ContactController.class)
-public class ContactControllerTest {
-
+@ContextConfiguration(classes = {ContactController.class})
+@ExtendWith(SpringExtension.class)
+@DisabledInAotMode
+class ContactControllerTest {
     @Autowired
-    private MockMvc mockMvc;
+    private ContactController contactController;
 
     @MockBean
     private ContactService contactService;
 
-    @InjectMocks
-    private ContactController contactController;
+    /**
+     * Test {@link ContactController#list()}.
+     * <p>
+     * Method under test: {@link ContactController#list()}
+     */
+    @Test
+    @DisplayName("Test list()")
+    void testList() throws Exception {
+        // Arrange
+        when(contactService.findAll()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/contacts");
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new ContactController(contactService)).build();
+        // Act and Assert
+        MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
+    /**
+     * Test {@link ContactController#get(Integer)}.
+     * <p>
+     * Method under test: {@link ContactController#get(Integer)}
+     */
     @Test
-
-
-    public void testGetContact() throws Exception {
-        Integer contactId = 1;
+    @DisplayName("Test get(Integer)")
+    void testGet() throws Exception {
+        // Arrange
         Contact contact = new Contact();
-        contact.setId(contactId);
-        when(contactService.findById(contactId)).thenReturn(contact);
-
-        mockMvc.perform(get("/api/contacts/" + contactId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(contactId));
-    }
-
-    @Test
-    public void testCreateContact() throws Exception {
-        ContactDTO contactDTO = new ContactDTO();
-        contactDTO.setName("New Contact");
-        contactDTO.setEmail("new@example.com");
-        Contact contact = new Contact();
+        contact.setCreatedAt(LocalDate.of(1970, 1, 1).atStartOfDay());
+        contact.setEmail("jane.doe@example.org");
         contact.setId(1);
-        contact.setName(contactDTO.getName());
-        contact.setEmail(contactDTO.getEmail());
+        contact.setName("Name");
+        when(contactService.findById(Mockito.<Integer>any())).thenReturn(contact);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/contacts/{id}", 1);
 
-        when(contactService.create(contactDTO)).thenReturn(contact);
-
-        mockMvc.perform(post("/api/contacts/new")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(contactDTO)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value("New Contact"));
+        // Act and Assert
+        MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"id\":1,\"name\":\"Name\",\"email\":\"jane.doe@example.org\",\"createdAt\":[1970,1,1,0,0]}"));
     }
 
+    /**
+     * Test {@link ContactController#create(ContactDTO)}.
+     * <ul>
+     *   <li>Given {@code jane.doe@example.org}.</li>
+     *   <li>Then status {@link StatusResultMatchers#isCreated()}.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link ContactController#create(ContactDTO)}
+     */
     @Test
-    public void testUpdateContact() throws Exception {
-        Integer contactId = 1;
+    @DisplayName("Test create(ContactDTO); given 'jane.doe@example.org'; then status isCreated()")
+    void testCreate_givenJaneDoeExampleOrg_thenStatusIsCreated() throws Exception {
+        // Arrange
+        Contact contact = new Contact();
+        contact.setCreatedAt(LocalDate.of(1970, 1, 1).atStartOfDay());
+        contact.setEmail("jane.doe@example.org");
+        contact.setId(1);
+        contact.setName("Name");
+        when(contactService.create(Mockito.<ContactDTO>any())).thenReturn(contact);
+
         ContactDTO contactDTO = new ContactDTO();
-        contactDTO.setName("Updated Contact");
-        contactDTO.setEmail("update@example.com");
-        Contact updatedContact = new Contact();
-        updatedContact.setId(contactId);
-        updatedContact.setName(contactDTO.getName());
-        updatedContact.setEmail(contactDTO.getEmail());
+        contactDTO.setEmail("jane.doe@example.org");
+        contactDTO.setName("Name");
+        String content = (new ObjectMapper()).writeValueAsString(contactDTO);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/contacts/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
 
-        when(contactService.update(contactId, contactDTO)).thenReturn(updatedContact);
+        // Act
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder);
 
-        mockMvc.perform(put("/api/contacts/update/" + contactId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(contactDTO)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("Updated Contact"));
+        // Assert
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"id\":1,\"name\":\"Name\",\"email\":\"jane.doe@example.org\",\"createdAt\":[1970,1,1,0,0]}"));
     }
 
+    /**
+     * Test {@link ContactController#create(ContactDTO)}.
+     * <ul>
+     *   <li>Given {@code U.U.U}.</li>
+     *   <li>When {@link ContactDTO} (default constructor) Email is
+     * {@code U.U.U}.</li>
+     *   <li>Then status four hundred.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link ContactController#create(ContactDTO)}
+     */
     @Test
-    public void testDeleteContact() throws Exception {
-        Integer contactId = 1;
+    @DisplayName("Test create(ContactDTO); given 'U.U.U'; when ContactDTO (default constructor) Email is 'U.U.U'; then status four hundred")
+    void testCreate_givenUUU_whenContactDTOEmailIsUUU_thenStatusFourHundred() throws Exception {
+        // Arrange
+        Contact contact = new Contact();
+        contact.setCreatedAt(LocalDate.of(1970, 1, 1).atStartOfDay());
+        contact.setEmail("jane.doe@example.org");
+        contact.setId(1);
+        contact.setName("Name");
+        when(contactService.create(Mockito.<ContactDTO>any())).thenReturn(contact);
 
-        doNothing().when(contactService).delete(contactId);
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setEmail("U.U.U");
+        contactDTO.setName("Name");
+        String content = (new ObjectMapper()).writeValueAsString(contactDTO);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/contacts/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
 
-        mockMvc.perform(delete("/api/contacts/delete/" + contactId))
-                .andExpect(status().isNoContent());
+        // Act
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder);
+
+        // Assert
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
     }
 
+    /**
+     * Test {@link ContactController#update(Integer, ContactDTO)}.
+     * <p>
+     * Method under test: {@link ContactController#update(Integer, ContactDTO)}
+     */
+    @Test
+    @DisplayName("Test update(Integer, ContactDTO)")
+    void testUpdate() throws Exception {
+        // Arrange
+        Contact contact = new Contact();
+        contact.setCreatedAt(LocalDate.of(1970, 1, 1).atStartOfDay());
+        contact.setEmail("jane.doe@example.org");
+        contact.setId(1);
+        contact.setName("Name");
+        when(contactService.update(Mockito.<Integer>any(), Mockito.<ContactDTO>any())).thenReturn(contact);
 
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setEmail("jane.doe@example.org");
+        contactDTO.setName("Name");
+        String content = (new ObjectMapper()).writeValueAsString(contactDTO);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/contacts/update/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
 
+        // Act and Assert
+        MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"id\":1,\"name\":\"Name\",\"email\":\"jane.doe@example.org\",\"createdAt\":[1970,1,1,0,0]}"));
+    }
+
+    /**
+     * Test {@link ContactController#delete(Integer)}.
+     * <p>
+     * Method under test: {@link ContactController#delete(Integer)}
+     */
+    @Test
+    @DisplayName("Test delete(Integer)")
+    void testDelete() throws Exception {
+        // Arrange
+        doNothing().when(contactService).delete(Mockito.<Integer>any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/api/contacts/delete/{id}", 1);
+
+        // Act
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(contactController)
+                .build()
+                .perform(requestBuilder);
+
+        // Assert
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
 }
